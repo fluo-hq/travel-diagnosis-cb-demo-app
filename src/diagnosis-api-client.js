a688368470b57f0918f508704327efc28f7eafd1
Laurent Callarec <laurent.callarec@fluo.com>
2018-06-01 16:08:02 +0200

    refactor(api-client): Handle missing configuration

--- src/diagnosis-api-client.js
+++ src/diagnosis-api-client.js
@@ -1,53 +1,49 @@
-const BASE_URL = "http://localhost:8080";
-const TOKEN = "TODO_MY_JWT_PARTNER_TOKEN";
+const FLUO_DIAGNOSIS_HOST = "INSERT THE FLUO DIAGNOSIS HOST HERE";
+const JWT_PARTNER_TOKEN = "INSERT YOUR JWT PARTNER TOKEN HERE";
+
+if(JWT_PARTNER_TOKEN === "INSERT YOUR JWT PARTNER TOKEN HERE" || FLUO_DIAGNOSIS_HOST === "INSERT THE FLUO DIAGNOSIS HOST HERE"){
+  throw new Error('Please configure your credentials according to the README.');
+}
+
+function getHeaders(){
+  return {
+    'Authorization': `Bearer ${JWT_PARTNER_TOKEN}`,
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Accept': 'application/json',
+  };
+}
 
 export async function fetchBanks() {
-  const response = await fetch(BASE_URL + '/diagnosis/payment_card/v1/banks', {
+  const response = await fetch(`${FLUO_DIAGNOSIS_HOST}/diagnosis/payment_card/v1/banks`, {
     method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + TOKEN,
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Accept': 'application/json',
-    }
+    headers: getHeaders()
   });
   
   return await response.json();
 }
 
 export async function fetchCardsByBank(bank) {
-  const response = await fetch(`${BASE_URL}/diagnosis/payment_card/v1/banks/${bank.id}/cards`, {
+  const response = await fetch(`${FLUO_DIAGNOSIS_HOST}/diagnosis/payment_card/v1/banks/${bank.id}/cards`, {
     method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + TOKEN,
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Accept': 'application/json',
-    }
+    headers: getHeaders()
   });
   
   return await response.json();
 }
 
 export async function fetchDestinations() {
-  const response = await fetch(`${BASE_URL}/diagnosis/v1/countries`, {
+  const response = await fetch(`${FLUO_DIAGNOSIS_HOST}/diagnosis/v1/countries`, {
     method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + TOKEN,
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Accept': 'application/json',
-    }
+    headers: 
   });
   
   return await response.json();
 }
 
 export async function fetchDiagnosis(card, destination) {
-  const response = await fetch(`${BASE_URL}/diagnosis/payment_card/v1/diagnoses`, {
+  const response = await fetch(`${FLUO_DIAGNOSIS_HOST}/diagnosis/payment_card/v1/diagnoses`, {
     method: 'POST',
-    headers: {
-      'Authorization': 'Bearer ' + TOKEN,
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
+    headers: getHeaders(),
     body: JSON.stringify({
       "contextType": "TRAVEL",
       "cardId": card.id,
